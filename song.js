bd89fa8193e73466570c0408736efe35378c0ac0
MAOstrander <mathew.ostrander@gmail.com>
2015-10-21 11:45:15 -0500

    got some style going!

--- song.js
+++ song.js
@@ -2,11 +2,13 @@ var songs = [];
 var outputSongs = [];
 var songInfo = document.getElementById("song-info");
 var songInput = document.getElementById("song-input");
+var controls = document.getElementById("controls");
 var buttonAdd = document.getElementById("button-add");
 
 var songAdd = document.getElementsByName("song-add");
 var artistAdd = document.getElementsByName("artist-add");
 var albumAdd = document.getElementsByName("album-add");
+var genreAdd = document.getElementsByName("genre-add");
 
 songs[songs.length] = "Rise - by Yoko Kanno on the album Ghost in the Shell OST";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
@@ -16,13 +18,17 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 songs[songs.length] = "Dare - by Gorillaz on the album Demon Days";
 
+function filterSong(song) {
+	song = song.replace(">", "-");
+	song = song.replace(/[&\/\\#,+()!@$~%.*?{}]/g, '');
+	console.log(song)
+	return song;
+}
 
 for (var i = 0; i < songs.length; i++) {
 	song = songs[i];
 
-	song = song.replace(">", "-");
-	song = song.replace(/[&\/\\#,+()!@$~%.*?{}]/g, '');
-	console.log(song)
+	song = filterSong(song);
 
 	var songName = song.slice(0, song.indexOf("-")-1);
 	var artistName = song.slice(song.indexOf("by")+3, song.indexOf("on the album"));
@@ -41,32 +47,32 @@ document.querySelector("body").addEventListener("click", function(event) {
 	console.log("Event:", event);
   // Handle the click event on my nav li
   if (event.target.id === "list") {
-	console.log("That's the list!");
 	songInput.classList.add("display-hide");
 	songInfo.classList.remove("display-hide");
-	console.log("SongInfo classList", songInfo.classList);
+	controls.classList.remove("display-hide");
 	}	else if (event.target.id === "add") {
-	console.log("That's the add!");
 	songInfo.classList.add("display-hide");
+	controls.classList.add("display-hide");
 	songInput.classList.remove("display-hide");
-	console.log("SongInput classList", songInput.classList);
 	}
 });
 
 function update() {
-	songName = songAdd[0].value;
-	artistName = artistAdd[0].value;
-	albumName = albumAdd[0].value;
-	outputSongs.push("<div class='song'><h2>" + songName + "</h2><ul><li>" + artistName + "</li><li class='middle'>" + albumName + "</li><li>Pop</li></ul></div>");
+	songName = filterSong(songAdd[0].value);
+	artistName = filterSong(artistAdd[0].value);
+	albumName = filterSong(albumAdd[0].value);
+	genreName = filterSong(genreAdd[0].value);
+	output = "<div class='song'><h2>" + songName + "</h2><ul><li>" + artistName + "</li><li class='middle'>" + albumName + "</li><li>" + genreName + "</li></ul></div>"
+	outputSongs.unshift(output);
 
 	songInfo.innerHTML = "";
 	for (var j = 0; j < outputSongs.length; j++) {
 	songInfo.innerHTML += outputSongs[j];
 	//Switch back to music list window
+	}
 	songInput.classList.add("display-hide");
 	songInfo.classList.remove("display-hide");
-	}
-
+	controls.classList.remove("display-hide");
 }
 
 buttonAdd.addEventListener("click", update);
