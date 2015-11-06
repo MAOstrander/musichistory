53b709597193aad75a64a08b71cf2e7b0d7d5af9
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-05 18:38:55 -0600

    craziness

--- javascripts/populate-songs.js
+++ javascripts/populate-songs.js
@@ -2,12 +2,32 @@ define(["jquery", "executeMe", 'hbs!../templates/songs'],
 	function($, executeMe, Handlebars) {
   return {
     fetchData: function() {
-      $.ajax({
-      	url: "data/songs.json"
-      	}).done(function(song) {
-			$("#song-info").html(Handlebars(song));
-			executeMe.executeMe(song);
-		});
+
+      var mySongs = new Firebase("https://mistory.firebaseio.com/");
+      mySongs.on("value", function(snapshot) {
+        var song = snapshot.child("user2").val();
+        console.log("playlist", song);
+        $("#song-info").html(Handlebars(song));
+        }
+      );
+
+
+      // function(snapshot) {
+      //   var playlist = snapshot.child("User").child("playlist1").val();
+      //   var q;
+      //   for (var i = 1; i < 3; i++) {
+      //   q = "song" + i + "";
+      //   console.log("q", q);
+      //   console.log("playlist", playlist[q].artist);
+      //   }
+      // });
+      
+  //     $.ajax({
+  //      url: "data/songs.json"
+  //      }).done(function(song) {
+     // $("#song-info").html(Handlebars(song));
+    //  executeMe.executeMe(song);
+		// });
     }
   };
 });
--- javascripts/song.js
+++ javascripts/song.js
@@ -13,60 +13,61 @@ define(["jquery", "lodash", "bootstrap", "executeMe", "updateMe"],
 	// // This takes a 'snapshot' of the database at the time you call it and allows you to traverse to any 
 	// // particular value like an object.
 	// mySongs.on("value", function(snapshot) {
-	//   console.log(snapshot.child("User").child("playlist1").child("song1").val());  // Alerts "Final Fantasy 7"
+	//   var playlist = snapshot.child("User").child("playlist1").val();
+	//   console.log("playlist", playlist);
 	// });
 
-var songs = [];
+	var songs = [];
 
-var songInfo = $("#song-info");
-var songInput = $("#song-input");
-var controls = $("#controls");
-var buttonAdd = $("#button-add");
-var buttonQuickAdd = $("#button-quick-add");
+	var songInfo = $("#song-info");
+	var songInput = $("#song-input");
+	var controls = $("#controls");
+	var buttonAdd = $("#button-add");
+	var buttonQuickAdd = $("#button-quick-add");
 
-// Listening for clicks
-$("body").click( function(event) {
-  // Handle the click event on my nav li
- 	if (event.target.id === "list") {
-		songInfo.show();
-		controls.show();
-		$("#maybe-art").show();
-		songInput.hide();
-	} else if (event.target.id === "add") {
-		songInput.show();
-		songInfo.hide();
-		controls.hide();
-		$("#maybe-art").hide();
+	// Listening for clicks
+	$("body").click( function(event) {
+	  // Handle the click event on my nav li
+	 	if (event.target.id === "list") {
+			songInfo.show();
+			controls.show();
+			$("#maybe-art").show();
+			songInput.hide();
+		} else if (event.target.id === "add") {
+			songInput.show();
+			songInfo.hide();
+			controls.hide();
+			$("#maybe-art").hide();
+		}
+	});
+
+	buttonAdd.attr("disabled", "true");
+	buttonQuickAdd.attr("disabled", "true");
+	var fieldInput = $(":text");
+	function validate(){
+		if (fieldInput.eq(0).val() !== "" && 
+			fieldInput.eq(1).val() !== "" && 
+			fieldInput.eq(2).val() !== "" && 
+			fieldInput.eq(3).val() !== "") {
+			buttonQuickAdd.removeAttr("disabled");
+		} else if (fieldInput.eq(4).val() !== "" && 
+			fieldInput.eq(5).val() !== "" && 
+			fieldInput.eq(6).val() !== "" && 
+			fieldInput.eq(7).val() !== "") {
+			buttonAdd.removeAttr("disabled");
+		} else {
+			buttonAdd.attr("disabled", "true");
+			buttonQuickAdd.attr("disabled", "true");
+		}
 	}
-});
+	fieldInput.keyup(validate);
 
-buttonAdd.attr("disabled", "true");
-buttonQuickAdd.attr("disabled", "true");
-var fieldInput = $(":text");
-function validate(){
-	if (fieldInput.eq(0).val() !== "" && 
-		fieldInput.eq(1).val() !== "" && 
-		fieldInput.eq(2).val() !== "" && 
-		fieldInput.eq(3).val() !== "") {
-		buttonQuickAdd.removeAttr("disabled");
-	} else if (fieldInput.eq(4).val() !== "" && 
-		fieldInput.eq(5).val() !== "" && 
-		fieldInput.eq(6).val() !== "" && 
-		fieldInput.eq(7).val() !== "") {
-		buttonAdd.removeAttr("disabled");
-	} else {
-		buttonAdd.attr("disabled", "true");
-		buttonQuickAdd.attr("disabled", "true");
-	}
-}
-fieldInput.keyup(validate);
+	buttonAdd.click(updateMe.updateMe);
+	buttonQuickAdd.click(updateMe.updateMe);
 
-buttonAdd.click(updateMe.updateMe);
-buttonQuickAdd.click(updateMe.updateMe);
-
-$(document).on('click', '.delete-song', function(event) {
-	$(this).parent().remove();
-});
+	$(document).on('click', '.delete-song', function(event) {
+		$(this).parent().remove();
+	});
 
 
 }); //END DEFINE FUNCTION
