8bb50ffde334f3acf51eec3455558384a8dafbc9
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-07 15:17:39 -0600

    things working

--- javascripts/alterPlaylist.js
+++ javascripts/alterPlaylist.js
@@ -1,16 +1,35 @@
-define(["jquery", "lodash"], function($, _) {
+define(["jquery", "lodash", 'hbs!../templates/songs'], 
+	function($, _, Handlebars) {
+
 	function timeToFilter (){
+		var filteredPlay = playlist.slice(0);
 		console.log("FILTER BUTTON PRESSED!");
 		// var filteredPlaylist = $(".song");
-		console.log("test for global playlist", playlist);
+		// console.log("did the slice work?", filteredPlay);
+
+		var artistSelect = $("[name='artist']").val();
+		var albumSelect = $("[name='album']").val();
+		console.log("albumSelect", albumSelect);
+
+	    function findArtist(value) {
+          return value.artist === artistSelect;
+        }
+        function findAlbum(value) {
+          return value.album === albumSelect;
+        }
+
+		if (artistSelect !== "") {
+         filteredPlay = playlist.filter(findArtist);
+		}
+		if (albumSelect !== "") {
+         filteredPlay = playlist.filter(findAlbum);
+		}
+		console.log("filteredPlay", filteredPlay);
+
+		$("#song-info").html(Handlebars(filteredPlay));
 	}
 
-	       //  function findArtist(value) {
-        //   return value.artist === "Garnet";
-        // }
-        //  var filteredPlay = playlist.filter(findArtist);
-        //  console.log("filteredPlay", filteredPlay);
-        //  console.log("playlist again", playlist);
+         // console.log("playlist again", playlist);
 
 	return {
 		timeToFilter: timeToFilter
